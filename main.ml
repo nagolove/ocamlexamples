c7300dc4136e41ab9ce77a8695d6dd160a2f864f
nagolove <dekar.own@gmail.com>
2021-07-04 06:50:09 +0000

    some repl stuff.

--- main.ml
+++ main.ml
@@ -1,2 +1,29 @@
 (*print_string ''Hello, World!\n'';;*)
+
+(*load "printf.ml"*)
+
+(*printf "text";;*)
+
+(*let k = 1+1;;*)
+(*k = k + 1;;*)
+(*mut z = k;;*)
+1-1;;
+1/1;;
+10/3;;
+(*13.1*10.0;;*)
+(*13*10.9;;*)
+
 print_string ", World!\n";;
+print_string ", уалцудкоа юникод юникод !\n";;
+
+(*print_int 4;;*)
+(*print_int (43 ^ 2);;*)
+(*print_int (43/2);;*)
+
+let a = 1;;
+let foo = "Mne ne ntarvitsya rust. Poprobuu pozhe.)";;
+let a = 2.2;;
+
+let foo = 'c';;
+
+1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000
