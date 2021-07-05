21c867b972255463254b02a619d9612981c75cc4
nagolove <dekar.own@gmail.com>
2021-07-05 19:17:28 +0000

    more shit.

--- main.ml
+++ main.ml
@@ -1,3 +1,25 @@
+(*
+ *for
+ *while
+ *fun
+ *int
+ *float
+ *char
+ *if
+ *then
+ *do
+ *)
+
+let x = 2 in x + 1;;
+let x = 3 in x + 1;;
+
+let x = 1.3;;
+let y = 1;;
+
+float_of_int y;;
+int_of_float x;;
+
+(*1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000*)
 (*print_string ''Hello, World!\n'';;*)
 
 (*load "printf.ml"*)
@@ -13,9 +35,6 @@
 (*13.1*10.0;;*)
 (*13*10.9;;*)
 
-print_string ", World!\n";;
-print_string ", уалцудкоа юникод юникод !\n";;
-
 (*print_int 4;;*)
 (*print_int (43 ^ 2);;*)
 (*print_int (43/2);;*)
@@ -26,4 +45,14 @@ let a = 2.2;;
 
 let foo = 'c';;
 
-1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000 1,000,000
+();;
+
+let msg = "Дикая строчка.";;
+let len = 300;;
+let pi = 3.14159265;;
+
+len;;
+
+print_string ", World!\n";;
+print_string ", уалцудкоа юникод юникод !\n";;
+
